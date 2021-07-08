a8e98f51ab43df0645931b0e9c356eb4520b908f
demetreades <demetreades@protonmail.com>
2021-07-08 15:29:27 +0300

    credentials

--- database/credentials.js
+++ database/credentials.js
@@ -1,15 +1,7 @@
-// const credentials = {
-//   host:     '127.0.0.1',
-//   // user:     'enter your MySQL username here',
-//   // password: 'enter your MySQL password here',
-//   database: 'small_school'
-// };
-
 const credentials = {
-  host:     '127.0.0.1',
-  user:     'root',
-  password: '1212',
-  port:     '3306',
+  host: '127.0.0.1',
+  // user:     'enter your MySQL username here',
+  // password: 'enter your MySQL password here',
   database: 'small_school'
 };
 
