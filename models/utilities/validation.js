8b3ed5d1df06031dd02ccaf85d5f7cb3382a05dd
demetreades <demetreades@protonmail.com>
2021-10-14 13:20:29 +0300

    new validation functions

--- models/utilities/validation.js
+++ models/utilities/validation.js
@@ -1,29 +1,47 @@
-function charTypeChecker (value, text) {
+const isValidCharacters = (value, text) => {
   const validCharacters = /^[Α-Ωα-ωA-Za-z]+$/;
-  if(!value.match(validCharacters)) {
-    throw new Error(`Invalid characters "${value}" inserted into ${text}`);
+  if (!value.match(validCharacters)) {
+    throw new Error(`Invalid characters: ${value} inserted into ${text}`);
   }
 };
 
-function numTypeChecker(value, text) {
-  if(isNaN(value)) {
-    throw new Error(`Invalid ${text} value of ${value} not a number`);
+const isEmpty = (value, text) => {
+  if (value === '' || value === undefined || value === null) {
+    throw new Error(`Invalid ${text} value: ${value} cannot be empty`);
   }
 };
-  
-function rangeChecker(value, min, max, text) {
+
+const isNumber = (value, text) => {
+  // typeof value !== 'number';
+  if (isNaN(value)) {
+    throw new Error(`Invalid ${text} value: ${value} not a number`);
+  }
+};
+
+const isString = (value, text) => {
+  if (typeof value !== 'string') {
+    throw new Error(`Invalid ${text} value: ${value} not a string`);
+  }
+};
+
+const isInRange = (value, min, max, text) => {
   if (min !== null && value < min) {
-    throw new Error(`Invalid ${text} value of ${value} precedes minimum range of ${min}`);
+    throw new Error(
+      `Invalid ${text} value of ${value} precedes minimum range of ${min}`
+    );
   }
-  
+
   if (max !== null && value > max) {
-    throw new Error(`Invalid ${text} value of ${value} exceeds maximum range of ${max}`);
+    throw new Error(
+      `Invalid ${text} value of ${value} exceeds maximum range of ${max}`
+    );
   }
 };
 
-
-module.exports = { 
-  charTypeChecker, 
-  numTypeChecker, 
-  rangeChecker,
+module.exports = {
+  isValidCharacters,
+  isNumber,
+  isString,
+  isInRange,
+  isEmpty,
 };
