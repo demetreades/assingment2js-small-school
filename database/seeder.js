ac503c97211ff63998386643cdb306da1b6c80a2
demetreades <demetreades@protonmail.com>
2021-10-14 14:24:50 +0300

    setTimeout added

--- database/seeder.js
+++ database/seeder.js
@@ -1,20 +1,12 @@
-const SQL_DATA = require('./data');
-
 const mysql = require('mysql2');
 
-const con = mysql.createConnection({
-  host: '127.0.0.1',
-  // user:     'enter your MySQL username here',
-  // password: 'enter your MySQL password here',
-});
+const SQL_DATA = require('./data');
+const credentials = require('./credentials');
+
+const con = mysql.createConnection(credentials);
 
 console.log(`\n------------------- Initializing -------------------
 ----------------------------------------------------\n`);
-
-databaseInit(SQL_DATA.schema);
-databaseInit(SQL_DATA.tables);
-databaseInit(SQL_DATA.views);
-databaseInit(SQL_DATA.inserts);
 
 function databaseInit(array) {
   let text = '';
@@ -22,19 +14,25 @@ function databaseInit(array) {
     if (err) throw err;
     else {
       array.forEach((query, index) => {
-        con.query(query, function (err) {
+        con.query(query, (err) => {
           if (err) throw err;
           if (array === SQL_DATA.schema) text = 'A: SQL_DATA.schema';
           if (array === SQL_DATA.tables) text = 'B: SQL_DATA.tables';
           if (array === SQL_DATA.views) text = 'C: SQL_DATA.views';
           if (array === SQL_DATA.inserts) text = 'D: SQL_DATA.inserts';
-          console.log(`${index + 1}:${text} - Successful!!`);
+          console.log(`${index + 1}:${text}`);
         });
       });
     }
   });
 }
 
+databaseInit(SQL_DATA.schema);
+databaseInit(SQL_DATA.tables);
+databaseInit(SQL_DATA.views);
+databaseInit(SQL_DATA.inserts);
+
 setTimeout(() => {
+  console.log('\nEXIT\n');
   process.exit(1);
 }, 2000);
