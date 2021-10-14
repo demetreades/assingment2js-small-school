6632b4d72a31bd03ed8504c583e9ff90fe9d3a00
demetreades <demetreades@protonmail.com>
2021-10-14 13:21:45 +0300

    logging, prettier styling

--- database/connection.js
+++ database/connection.js
@@ -1,27 +1,34 @@
 const mysql = require('mysql2');
-const credentials = require('./credentials').credentials;
-
+const { credentials } = require('./credentials');
 
 async function sqlConnection(sql) {
   const con = mysql.createConnection(credentials);
   return await new Promise((resolve, reject) => {
-    con.connect( (err) => {
-      if(err) console.log("\n-------------------- Error Connecting! --------------------\n\n", err);
+    con.connect((err) => {
+      if (err)
+        console.log(
+          '\n-------------------- Error Connecting! --------------------\n\n',
+          err
+        );
       else {
-        console.log("\n--------------------- SQL Connected ----------------------\n");
+        console.log(
+          '\n--------------------- SQL Connected ----------------------\n'
+        );
         con.query(sql, (err, result) => {
-          if(err) throw err;
+          if (err) throw err;
+          console.log('SQL CONNECTION ERROR: ', err);
           resolve(result);
         });
         con.end((err) => {
-          if(err) throw err;
-          console.log("-------------------- SQL Disconnected --------------------\n");
+          if (err) throw err;
+          console.log('SQL CONNECTION-END ERROR: ', err);
+          console.log(
+            '-------------------- SQL Disconnected --------------------\n'
+          );
         });
       }
     });
   });
-};
-
+}
 
 module.exports = { sqlConnection };
-
