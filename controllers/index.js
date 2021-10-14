acfd2476cd1c217799e2bcfd3247d9376eed2b51
demetreades <demetreades@protonmail.com>
2021-10-14 13:31:53 +0300

    refactor

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function (req, res) {
+router.get('/', (req, res) => {
   res.render('index', {
     title: 'Express CRUD small project',
     course: 'Javascríptà No Time',
